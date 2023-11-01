af2c515a6472d0cd8f17849596fc311af7fc4e1e
Priscilla Uchoa Martins <pristark@gmail.com>
2023-11-01 09:55:06 -0300

    Update Menu.py

--- analise_gastos_saude/streamlit/Menu.py
+++ analise_gastos_saude/streamlit/Menu.py
@@ -2,7 +2,6 @@ from pathlib import Path
 import streamlit as st
 import pandas as pd
 import plotly.express as px
-#from streamlit_option_menu import option_menu
 from st_pages import Page, Section, add_page_title, show_pages
 
 
