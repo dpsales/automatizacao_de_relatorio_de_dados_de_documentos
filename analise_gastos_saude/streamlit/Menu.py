b4afe727ba9c3434e68e75a7b52c061b33daedcb
dpsales <8014899+dpsales@users.noreply.github.com>
2023-11-01 05:07:59 -0300

    tentando

--- analise_gastos_saude/streamlit/Menu.py
+++ analise_gastos_saude/streamlit/Menu.py
@@ -1,7 +1,7 @@
 from pathlib import Path
 import streamlit as st
-import pandas as pd
-import plotly.express as px
+#import pandas as pd
+#import plotly.express as px
 #from streamlit_option_menu import option_menu
 from st_pages import Page, Section, add_page_title, show_pages
 #"Projeto Final do Bootcamp de Análise de Dados - ENAP "
